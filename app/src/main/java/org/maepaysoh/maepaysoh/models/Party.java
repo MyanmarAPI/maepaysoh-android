9078f5c05a9f04ff1b13e37c58e958d76997ccce
Ye Lin Aung <me@yelinaung.com>
2015-08-03 19:09:30 +0630

    added Party model

---
+++ app/src/main/java/org/maepaysoh/maepaysoh/models/Party.java
@@ -0,0 +1,206 @@
+package org.maepaysoh.maepaysoh.models;
+
+/**
+ * Created by Ye Lin Aung on 15/08/03.
+ */
+public class Party {
+
+  private String PartyName;
+  private String PartyNameEnglish;
+  private String EstablishmentDate;
+  private String MemberCount;
+  private String Leadership;
+  private String EstablishmentApprovalDate;
+  private String RegistrationApplicationDate;
+  private String RegistrationApprovalDate;
+  private String ApprovedPartyNumber;
+  private String PartyFlag;
+  private String PartySeal;
+  private String Chairman;
+  private String Region;
+  private String Headquarters;
+  private String Contact;
+  private String Policy;
+
+  /**
+   * @return The PartyName
+   */
+  public String getPartyName() {
+    return PartyName;
+  }
+
+  /**
+   * @param PartyName The PartyName
+   */
+  public void setPartyName(String PartyName) {
+    this.PartyName = PartyName;
+  }
+
+  /**
+   * @return The PartyNameEnglish
+   */
+  public String getPartyNameEnglish() {
+    return PartyNameEnglish;
+  }
+
+  /**
+   * @param PartyNameEnglish The PartyNameEnglish
+   */
+  public void setPartyNameEnglish(String PartyNameEnglish) {
+    this.PartyNameEnglish = PartyNameEnglish;
+  }
+
+  /**
+   * @return The EstablishmentDate
+   */
+  public String getEstablishmentDate() {
+    return EstablishmentDate;
+  }
+
+  /**
+   * @param EstablishmentDate The EstablishmentDate
+   */
+  public void setEstablishmentDate(String EstablishmentDate) {
+    this.EstablishmentDate = EstablishmentDate;
+  }
+
+  /**
+   * @return The MemberCount
+   */
+  public String getMemberCount() {
+    return MemberCount;
+  }
+
+  /**
+   * @param MemberCount The MemberCount
+   */
+  public void setMemberCount(String MemberCount) {
+    this.MemberCount = MemberCount;
+  }
+
+  /**
+   * @return The Leadership
+   */
+  public String getLeadership() {
+    return Leadership;
+  }
+
+  /**
+   * @param Leadership The Leadership
+   */
+  public void setLeadership(String Leadership) {
+    this.Leadership = Leadership;
+  }
+
+  /**
+   * @return The EstablishmentApprovalDate
+   */
+  public String getEstablishmentApprovalDate() {
+    return EstablishmentApprovalDate;
+  }
+
+  /**
+   * @param EstablishmentApprovalDate The EstablishmentApprovalDate
+   */
+  public void setEstablishmentApprovalDate(String EstablishmentApprovalDate) {
+    this.EstablishmentApprovalDate = EstablishmentApprovalDate;
+  }
+
+  /**
+   * @return The RegistrationApplicationDate
+   */
+  public String getRegistrationApplicationDate() {
+    return RegistrationApplicationDate;
+  }
+
+  /**
+   * @param RegistrationApplicationDate The RegistrationApplicationDate
+   */
+  public void setRegistrationApplicationDate(String RegistrationApplicationDate) {
+    this.RegistrationApplicationDate = RegistrationApplicationDate;
+  }
+
+  /**
+   * @return The RegistrationApprovalDate
+   */
+  public String getRegistrationApprovalDate() {
+    return RegistrationApprovalDate;
+  }
+
+  /**
+   * @param RegistrationApprovalDate The RegistrationApprovalDate
+   */
+  public void setRegistrationApprovalDate(String RegistrationApprovalDate) {
+    this.RegistrationApprovalDate = RegistrationApprovalDate;
+  }
+
+  /**
+   * @return The ApprovedPartyNumber
+   */
+  public String getApprovedPartyNumber() {
+    return ApprovedPartyNumber;
+  }
+
+  /**
+   * @param ApprovedPartyNumber The ApprovedPartyNumber
+   */
+  public void setApprovedPartyNumber(String ApprovedPartyNumber) {
+    this.ApprovedPartyNumber = ApprovedPartyNumber;
+  }
+
+  /**
+   * @return The PartyFlag
+   */
+  public String getPartyFlag() {
+    return PartyFlag;
+  }
+
+  /**
+   * @param PartyFlag The PartyFlag
+   */
+  public void setPartyFlag(String PartyFlag) {
+    this.PartyFlag = PartyFlag;
+  }
+
+  /**
+   * @return The PartySeal
+   */
+  public String getPartySeal() {
+    return PartySeal;
+  }
+
+  /**
+   * @param PartySeal The PartySeal
+   */
+  public void setPartySeal(String PartySeal) {
+    this.PartySeal = PartySeal;
+  }
+
+  /**
+   * @return The Chairman
+   */
+  public String getChairman() {
+    return Chairman;
+  }
+
+  /**
+   * @param Chairman The Chairman
+   */
+  public void setChairman(String Chairman) {
+    this.Chairman = Chairman;
+  }
+
+  /**
+   * @return The Region
+   */
+  public String getRegion() {
+    return Region;
+  }
+
+  /**
+   * @param Region The Region
+   */
+  public void setRegion(String Region) {
+    this.Region = Region;
+  }
+}
