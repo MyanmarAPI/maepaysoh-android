bb1000849ef4ac718d096ec996a3441ed4c005da
Ye Lin Aung <me@yelinaung.com>
2015-08-04 00:02:50 +0630

    Added Home activity elements.

--- app/src/main/java/org/maepaysoh/maepaysoh/ui/HomeActivity.java
+++ app/src/main/java/org/maepaysoh/maepaysoh/ui/HomeActivity.java
@@ -1,15 +1,36 @@
 package org.maepaysoh.maepaysoh.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 import org.maepaysoh.maepaysoh.R;
 
 /**
  * Created by Ye Lin Aung on 15/08/03.
  */
-public class HomeActivity extends BaseActivity {
-  @Override public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-    super.onCreate(savedInstanceState, persistentState);
+public class HomeActivity extends AppCompatActivity {
+  private Toolbar mToolBar;
+  private View mToolBarShadow;
+  private Button mPartyListBtn;
+
+  @Override public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_home);
+
+    mToolBar = (Toolbar) findViewById(R.id.home_toolbar);
+    mToolBarShadow = findViewById(R.id.home_toolbar_shadow);
+    mPartyListBtn = (Button) findViewById(R.id.home_party_list_btn);
+
+    mToolBar.setTitle(getString(R.string.app_name));
+
+    mPartyListBtn.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        Intent goToPartyList = new Intent(HomeActivity.this, PartyListActivity.class);
+        startActivity(goToPartyList);
+      }
+    });
   }
 }
