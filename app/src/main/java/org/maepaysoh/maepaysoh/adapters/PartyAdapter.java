7a7375dd5124699fcdb0776805624ec62132ee65
Ye Myat Thu <thu.yemyat@gmail.com>
2015-08-04 12:43:35 +0630

    Update party adapter

--- app/src/main/java/org/maepaysoh/maepaysoh/adapters/PartyAdapter.java
+++ app/src/main/java/org/maepaysoh/maepaysoh/adapters/PartyAdapter.java
@@ -1,29 +1,64 @@
 package org.maepaysoh.maepaysoh.adapters;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.bumptech.glide.Glide;
+import java.util.List;
+import org.maepaysoh.maepaysoh.R;
+import org.maepaysoh.maepaysoh.models.Party;
 
 /**
  * Created by Ye Lin Aung on 15/08/04.
  */
 public class PartyAdapter extends RecyclerView.Adapter<PartyAdapter.PartyViewHolder> {
+  private Context mContext;
+  private List<Party> mParties;
+  public PartyAdapter(){
 
+  }
+
+  public void setParties(List<Party> parties){
+    mParties = parties;
+    notifyDataSetChanged();
+  }
   @Override public PartyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-    return null;
+    mContext = parent.getContext();
+    View view = LayoutInflater.from(mContext)
+        .inflate(R.layout.party_item_view,parent,false);
+    return new PartyViewHolder(view);
   }
 
   @Override public void onBindViewHolder(PartyViewHolder holder, int position) {
-
+    Party party = mParties.get(position);
+    holder.mPartyNameMyanmar.setText(party.getPartyName());
+    holder.mPartyNameEnglish.setText(party.getPartyNameEnglish());
+    holder.mPartyLeader.setText(party.getLeadership());
+    Glide.with(mContext)
+        .load(party.getPartyFlag())
+        .centerCrop()
+        .into(holder.mPartyFlag);
   }
 
   @Override public int getItemCount() {
-    return 0;
+    return mParties != null?mParties.size():0;
   }
 
   class PartyViewHolder extends RecyclerView.ViewHolder {
+    private TextView mPartyNameEnglish;
+    private TextView mPartyNameMyanmar;
+    private TextView mPartyLeader;
+    private ImageView mPartyFlag;
     public PartyViewHolder(View itemView) {
       super(itemView);
+      mPartyNameEnglish = (TextView) itemView.findViewById(R.id.party_name_english);
+      mPartyNameMyanmar = (TextView) itemView.findViewById(R.id.party_name_myanmar);
+      mPartyLeader = (TextView) itemView.findViewById(R.id.party_leader);
+      mPartyFlag = (ImageView) itemView.findViewById(R.id.party_flag);
     }
   }
 }
