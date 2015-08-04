79989df373af5e0c05158c36f42da43dac095738
Ye Myat Thu <thu.yemyat@gmail.com>
2015-08-04 11:04:40 +0630

    Remove query param from Party Service

--- app/src/main/java/org/maepaysoh/maepaysoh/api/PartyService.java
+++ app/src/main/java/org/maepaysoh/maepaysoh/api/PartyService.java
@@ -4,7 +4,6 @@ import java.util.List;
 import org.maepaysoh.maepaysoh.models.Party;
 import retrofit.Callback;
 import retrofit.http.GET;
-import retrofit.http.Query;
 
 /**
  * Created by Ye Lin Aung on 15/08/04.
@@ -12,7 +11,6 @@ import retrofit.http.Query;
 public interface PartyService {
   @GET("/party")
   void listParties(
-      @Query("token") String token,
       Callback<List<Party>> partyCallback
   );
 }
