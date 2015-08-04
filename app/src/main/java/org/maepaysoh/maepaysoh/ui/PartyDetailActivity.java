0458005533cccca696ea828edd5f4e2b2602adb7
Ye Myat Thu <thu.yemyat@gmail.com>
2015-08-04 14:38:26 +0630

    Add blank party detail activity

---
+++ app/src/main/java/org/maepaysoh/maepaysoh/ui/PartyDetailActivity.java
@@ -0,0 +1,9 @@
+package org.maepaysoh.maepaysoh.ui;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by yemyatthu on 8/4/15.
+ */
+public class PartyDetailActivity extends AppCompatActivity {
+}
