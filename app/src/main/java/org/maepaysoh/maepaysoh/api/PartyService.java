ff31315b795ba3a9738e89426c3414282adffd2f
Ye Lin Aung <me@yelinaung.com>
2015-08-04 10:23:42 +0630

    Added PartyService

--- app/src/main/java/org/maepaysoh/maepaysoh/api/PartyService.java
+++ app/src/main/java/org/maepaysoh/maepaysoh/api/PartyService.java
@@ -1,7 +1,18 @@
 package org.maepaysoh.maepaysoh.api;
 
+import java.util.List;
+import org.maepaysoh.maepaysoh.models.Party;
+import retrofit.Callback;
+import retrofit.http.GET;
+import retrofit.http.Query;
+
 /**
  * Created by Ye Lin Aung on 15/08/04.
  */
 public interface PartyService {
+  @GET("/party")
+  void listParties(
+      @Query("token") String token,
+      Callback<List<Party>> partyCallback
+  );
 }
