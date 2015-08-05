2653bc3287ea6abd79e3ef3b64f6a7004e90b6e8
Ye Myat Thu <thu.yemyat@gmail.com>
2015-08-05 10:53:01 +0630

    Add query parameters for Get Candidate List service

--- app/src/main/java/org/maepaysoh/maepaysoh/api/CandidateService.java
+++ app/src/main/java/org/maepaysoh/maepaysoh/api/CandidateService.java
@@ -3,10 +3,12 @@ package org.maepaysoh.maepaysoh.api;
 import org.maepaysoh.maepaysoh.models.Candidate;
 import retrofit.Callback;
 import retrofit.http.GET;
+import retrofit.http.Query;
 
 /**
  * Created by Ye Lin Aung on 15/08/04.
  */
 public interface CandidateService {
-  @GET("/candidate/list") void listCandidates(Callback<Candidate> candidateCallback);
+  @GET("/candidate/list") void listCandidates(@Query("_with") String with,@Query("font") String font,@Query("per_page") String perpage,
+      Callback<Candidate> candidateCallback);
 }
