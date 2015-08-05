272eca960b2c5849a2f196f1fec0e30bef425bd7
Ye Myat Thu <thu.yemyat@gmail.com>
2015-08-05 11:23:28 +0630

    Implement pagination for candidate list

--- app/src/main/java/org/maepaysoh/maepaysoh/ui/CandidateListActivity.java
+++ app/src/main/java/org/maepaysoh/maepaysoh/ui/CandidateListActivity.java
@@ -11,9 +11,12 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.maepaysoh.maepaysoh.R;
 import org.maepaysoh.maepaysoh.adapters.CandidateAdapter;
+import org.maepaysoh.maepaysoh.adapters.EndlessRecyclerViewAdapter;
 import org.maepaysoh.maepaysoh.api.CandidateService;
 import org.maepaysoh.maepaysoh.api.RetrofitHelper;
 import org.maepaysoh.maepaysoh.models.Candidate;
@@ -23,6 +26,8 @@ import retrofit.Callback;
 import retrofit.RestAdapter;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
+
+import static org.maepaysoh.maepaysoh.api.CandidateService.PARAM_TYPE.*;
 
 /**
  * Created by Ye Lin Aung on 15/08/04.
@@ -42,7 +47,8 @@ public class CandidateListActivity extends BaseActivity {
   private List<CandidateData> mCandidateDatas;
   private LinearLayoutManager mLayoutManager;
   private CandidateAdapter mCandidateAdapter;
-
+  private EndlessRecyclerViewAdapter mEndlessRecyclerViewAdapter;
+  private int mCurrentPage = 1;
   @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_candidate_list);
@@ -69,32 +75,19 @@ public class CandidateListActivity extends BaseActivity {
     viewUtils = new ViewUtils(this);
     viewUtils.showProgress(mCandidateListRecyclerView, mProgressView, true);
 
-    LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
+    mLayoutManager = new LinearLayoutManager(this);
     mCandidateListRecyclerView.setLayoutManager(mLayoutManager);
     mCandidateAdapter = new CandidateAdapter();
-    mCandidateListRecyclerView.setAdapter(mCandidateAdapter);
+    mEndlessRecyclerViewAdapter = new EndlessRecyclerViewAdapter(this, mCandidateAdapter, new EndlessRecyclerViewAdapter.RequestToLoadMoreListener() {
+      @Override public void onLoadMoreRequested() {
+        loadData();
+      }
+    });
+    mCandidateListRecyclerView.setAdapter(mEndlessRecyclerViewAdapter);
 
     mCandidateRestAdapter = RetrofitHelper.getResAdapter();
     mCandidateListService = mCandidateRestAdapter.create(CandidateService.class);
-    mCandidateListService.listCandidates(new Callback<Candidate>() {
-      @Override public void success(Candidate returnObject, Response response) {
-
-        // Hide Progress on success
-        viewUtils.showProgress(mCandidateListRecyclerView, mProgressView, false);
-        switch (response.getStatus()) {
-          case 200:
-            mCandidateDatas = returnObject.getData();
-            mCandidateAdapter.setCandidates(mCandidateDatas);
-            Log.i("candidate", "total candidate : " + returnObject.getData().size());
-            break;
-        }
-      }
-
-      @Override public void failure(RetrofitError error) {
-        // Hide Progress on failure too
-        viewUtils.showProgress(mCandidateListRecyclerView, mProgressView, false);
-      }
-    });
+    loadData();
   }
 
   @Override public boolean onOptionsItemSelected(MenuItem item) {
@@ -104,4 +97,41 @@ public class CandidateListActivity extends BaseActivity {
     }
     return super.onOptionsItemSelected(item);
   }
+
+  private void loadData(){
+    Map<CandidateService.PARAM_TYPE,String> options= new HashMap<>();
+    options.put(_with,"party");
+    options.put(page,String.valueOf(mCurrentPage));
+    mCandidateListService.listCandidates(options,new Callback<Candidate>() {
+      @Override public void success(Candidate returnObject, Response response) {
+
+        // Hide Progress on success
+        viewUtils.showProgress(mCandidateListRecyclerView, mProgressView, false);
+        switch (response.getStatus()) {
+          case 200:
+            if(returnObject.getData()!=null && returnObject.getData().size() >0) {
+              if(mCurrentPage==1) {
+                mCandidateDatas = returnObject.getData();
+              }else{
+                mCandidateDatas.addAll(returnObject.getData());
+              }
+              mCandidateAdapter.setCandidates(mCandidateDatas);
+              mEndlessRecyclerViewAdapter.onDataReady(true);
+              mCurrentPage++;
+            }else{
+              mEndlessRecyclerViewAdapter.onDataReady(false);
+            }
+            Log.i("candidate", "total candidate : " + returnObject.getData().size());
+            break;
+        }
+      }
+
+      @Override public void failure(RetrofitError error) {
+        // Hide Progress on failure too
+        viewUtils.showProgress(mCandidateListRecyclerView, mProgressView, false);
+        mEndlessRecyclerViewAdapter.onDataReady(false);
+      }
+    });
+  }
+
 }
