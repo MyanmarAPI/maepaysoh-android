b76809964ca9d2f47afb1ba3220e19c6ea30dcf7
Ye Lin Aung <me@yelinaung.com>
2015-08-06 11:28:44 +0630

    Map faq ui ids to the objects

--- app/src/main/java/org/maepaysoh/maepaysoh/ui/FaqListActivity.java
+++ app/src/main/java/org/maepaysoh/maepaysoh/ui/FaqListActivity.java
@@ -1,23 +1,44 @@
 package org.maepaysoh.maepaysoh.ui;
 
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 import org.maepaysoh.maepaysoh.R;
+import org.maepaysoh.maepaysoh.utils.ViewUtils;
 
 /**
  * Created by Ye Lin Aung on 15/08/06.
  */
 public class FaqListActivity extends BaseActivity {
+
+  private RecyclerView mFaqListRecyclerView;
+  private ProgressBar mProgressView;
+  private View mErrorView;
+  private Button mRetryBtn;
+  private ViewUtils viewUtils;
+
   @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_faq_list);
 
     Toolbar mToolbar = (Toolbar) findViewById(R.id.faq_list_toolbar);
     View mToolbarShadow = findViewById(R.id.faq_list_toolbar_shadow);
+
+    mErrorView = findViewById(R.id.faq_list_error_view);
+    mFaqListRecyclerView = (RecyclerView) findViewById(R.id.faq_list_recycler_view);
+    mProgressView = (ProgressBar) findViewById(R.id.faq_list_progress_bar);
+    mRetryBtn = (Button) mErrorView.findViewById(R.id.error_view_retry_btn);
+
+    mProgressView.getIndeterminateDrawable()
+        .setColorFilter(getResources().getColor(R.color.primary), PorterDuff.Mode.SRC_ATOP);
+
     mToolbar.setTitle(getString(R.string.FaqList));
     hideToolBarShadowForLollipop(mToolbar, mToolbarShadow);
 
@@ -28,6 +49,8 @@ public class FaqListActivity extends BaseActivity {
       // Showing Back Arrow  <-
       mActionBar.setDisplayHomeAsUpEnabled(true);
     }
+
+    viewUtils = new ViewUtils(this);
   }
 
   @Override public boolean onOptionsItemSelected(MenuItem item) {
