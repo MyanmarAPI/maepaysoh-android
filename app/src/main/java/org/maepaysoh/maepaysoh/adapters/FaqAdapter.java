57af638fd175ebb5c6c973963c5121609c9c542f
Ye Myat Thu <thu.yemyat@gmail.com>
2015-08-06 12:14:35 +0630

    faq item layout

---
+++ app/src/main/java/org/maepaysoh/maepaysoh/adapters/FaqAdapter.java
@@ -0,0 +1,21 @@
+package org.maepaysoh.maepaysoh.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by yemyatthu on 8/6/15.
+ */
+public class FaqAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+  @Override public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    return null;
+  }
+
+  @Override public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+  }
+
+  @Override public int getItemCount() {
+    return 0;
+  }
+}
