39474d4462ef03c2172d868d2296e21a702fc876
Ye Lin Aung <me@yelinaung.com>
2015-08-06 11:29:54 +0630

    RestAdapter setLogLevel shall only be available if the app is in debug state

--- app/src/main/java/org/maepaysoh/maepaysoh/api/RetrofitHelper.java
+++ app/src/main/java/org/maepaysoh/maepaysoh/api/RetrofitHelper.java
@@ -1,6 +1,7 @@
 package org.maepaysoh.maepaysoh.api;
 
 import com.squareup.okhttp.OkHttpClient;
+import org.maepaysoh.maepaysoh.BuildConfig;
 import retrofit.RequestInterceptor;
 import retrofit.RestAdapter;
 import retrofit.client.OkClient;
@@ -12,14 +13,25 @@ public class RetrofitHelper {
   private static String token = "3db8827d-2521-57be-987a-e9e366874d4b";
 
   public static RestAdapter getResAdapter() {
-    return new RestAdapter.Builder().setClient(new OkClient(new OkHttpClient()))
-        .setEndpoint("http://api.maepaysoh.org")
-        .setLogLevel(RestAdapter.LogLevel.BASIC)
-        .setRequestInterceptor(new RequestInterceptor() {
-          @Override public void intercept(RequestFacade request) {
-            request.addQueryParam("token", token);
-          }
-        })
-        .build();
+    if (BuildConfig.DEBUG) {
+      return new RestAdapter.Builder().setClient(new OkClient(new OkHttpClient()))
+          .setEndpoint("http://api.maepaysoh.org")
+          .setLogLevel(RestAdapter.LogLevel.BASIC)
+          .setRequestInterceptor(new RequestInterceptor() {
+            @Override public void intercept(RequestFacade request) {
+              request.addQueryParam("token", token);
+            }
+          })
+          .build();
+    } else {
+      return new RestAdapter.Builder().setClient(new OkClient(new OkHttpClient()))
+          .setEndpoint("http://api.maepaysoh.org")
+          .setRequestInterceptor(new RequestInterceptor() {
+            @Override public void intercept(RequestFacade request) {
+              request.addQueryParam("token", token);
+            }
+          })
+          .build();
+    }
   }
 }
