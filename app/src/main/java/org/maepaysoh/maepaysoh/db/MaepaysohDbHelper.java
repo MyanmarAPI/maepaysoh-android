8e554c4fc314a1f65206bab1a0dd56514998b6c1
Ye Lin Aung <me@yelinaung.com>
2015-08-07 11:59:57 +0630

    Added columns for Party table

--- app/src/main/java/org/maepaysoh/maepaysoh/db/MaepaysohDbHelper.java
+++ app/src/main/java/org/maepaysoh/maepaysoh/db/MaepaysohDbHelper.java
@@ -9,6 +9,25 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 public class MaepaysohDbHelper extends SQLiteOpenHelper {
 
+  // Party
+  public static final String TABLE_NAME_PARTY = "parties";
+  public static final String COLUMN_PARTY_ID = "_id";
+  public static final String COLUMN_PARTY_NAME = "name";
+  public static final String COLUMN_PARTY_ESTABLISHMENT_DATE = "establishment_date";
+  public static final String COLUMN_PARTY_MEMBER_COUNT = "member_count";
+  public static final String COLUMN_PARTY_ESTABLISHMENT_APPROVAL_DATE =
+      "establishment_approval_date";
+  public static final String COLUMN_PARTY_REGISTRATION_APPLICATION_DATE =
+      "registration_application_date";
+  public static final String COLUMN_PARTY_REGISTRATION_APPROVAL_DATE = "registration_approval_date";
+  public static final String COLUMN_PARTY_APPROVED_PARTY_NUMBER = "approved_party_number";
+  public static final String COLUMN_PARTY_PARTY_FLAG = "party_flag";
+  public static final String COLUMN_PARTY_PARTY_SEAL = "party_seal";
+  public static final String COLUMN_PARTY_CHAIRMAN = "chairman";
+  public static final String COLUMN_PARTY_REGION = "region";
+  public static final String COLUMN_PARTY_CONTACT = "contact";
+  public static final String COLUMN_PARTY_POLICY = "policy";
+
   public static final int DATABASE_VERSION = 1;
   public static final String DATABASE_NAME = "maepaysoh.db";
 
