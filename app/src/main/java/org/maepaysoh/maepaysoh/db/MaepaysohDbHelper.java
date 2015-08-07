3d7999d698b0047cd8fe7cfc0c7fc9da96924ea6
Ye Myat Thu <thu.yemyat@gmail.com>
2015-08-07 16:47:02 +0630

    Add FAQ table on DB helper

--- app/src/main/java/org/maepaysoh/maepaysoh/db/MaepaysohDbHelper.java
+++ app/src/main/java/org/maepaysoh/maepaysoh/db/MaepaysohDbHelper.java
@@ -8,6 +8,10 @@ import android.database.sqlite.SQLiteOpenHelper;
  * Created by Ye Lin Aung on 15/08/07.
  */
 public class MaepaysohDbHelper extends SQLiteOpenHelper {
+  public static final int DATABASE_VERSION = 1;
+  public static final String DATABASE_NAME = "maepaysoh.db";
+  private static final String COMMA_SEP = ",";
+  private static final String TEXT_TYPE = " TEXT";
 
   // Party
   public static final String TABLE_NAME_PARTY = "parties";
@@ -30,10 +34,6 @@ public class MaepaysohDbHelper extends SQLiteOpenHelper {
   public static final String COLUMN_PARTY_POLICY = "policy";
   public static final String COLUMN_PARTY_HEADQUARTER = "party_headquarter";
   public static final String COLUMN_PARTY_LEADERSHIP = "leadership";
-  public static final int DATABASE_VERSION = 1;
-  public static final String DATABASE_NAME = "maepaysoh.db";
-  private static final String COMMA_SEP = ",";
-  private static final String TEXT_TYPE = " TEXT";
 
   private static final String SQL_CREATE_PARTY_TABLE =
       "CREATE TABLE " + MaepaysohDbHelper.TABLE_NAME_PARTY + " (" +
@@ -59,16 +59,42 @@ public class MaepaysohDbHelper extends SQLiteOpenHelper {
   private static final String SQL_DELETE_PARTIES =
       "DROP TABLE IF EXISTS " + MaepaysohDbHelper.TABLE_NAME_PARTY;
 
+  //FAQs
+  public static final String TABLE_NAME_FAQ = "faqs";
+  public static final String COLUMN_FAQ_ID = "_id";
+  public static final String COLUMN_FAQ_question = "question";
+  public static final String COLUMN_FAQ_answer = "answer";
+  public static final String COLUMN_FAQ_type = "type";
+  public static final String COLUMN_FAQ_basis = "basis";
+  public static final String COLUMN_FAQ_sections = "sections";
+  public static final String COLUMN_FAQ_url = "url";
+
+  private static final String SQL_CREATE_FAQ_TABLE =
+      "CREATE TABLE " + MaepaysohDbHelper.TABLE_NAME_FAQ + " (" +
+          MaepaysohDbHelper.COLUMN_FAQ_ID + TEXT_TYPE + COMMA_SEP +
+          MaepaysohDbHelper.COLUMN_FAQ_question + TEXT_TYPE + COMMA_SEP +
+          MaepaysohDbHelper.COLUMN_FAQ_answer + TEXT_TYPE + COMMA_SEP +
+          MaepaysohDbHelper.COLUMN_FAQ_basis + TEXT_TYPE + COMMA_SEP +
+          MaepaysohDbHelper.COLUMN_FAQ_type + TEXT_TYPE + COMMA_SEP +
+          MaepaysohDbHelper.COLUMN_FAQ_sections + TEXT_TYPE + COMMA_SEP +
+          MaepaysohDbHelper.COLUMN_FAQ_url + TEXT_TYPE + COMMA_SEP +
+          " )";
+
+  private static final String SQL_DELETE_FAQS =
+      "DROP TABLE IF EXISTS " + MaepaysohDbHelper.TABLE_NAME_FAQ;
+
   public MaepaysohDbHelper(Context context) {
     super(context, DATABASE_NAME, null, DATABASE_VERSION);
   }
 
   @Override public void onCreate(SQLiteDatabase db) {
     db.execSQL(SQL_CREATE_PARTY_TABLE);
+    db.execSQL(SQL_CREATE_FAQ_TABLE);
   }
 
   @Override public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
     db.execSQL(SQL_DELETE_PARTIES);
+    db.execSQL(SQL_DELETE_FAQS);
     onCreate(db);
   }
 }
