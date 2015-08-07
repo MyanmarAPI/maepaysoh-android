93a26760c98df2a698169c7e603b422ddd047989
Ye Myat Thu <thu.yemyat@gmail.com>
2015-08-07 13:12:14 +0630

    Add missing column

--- app/src/main/java/org/maepaysoh/maepaysoh/db/MaepaysohDbHelper.java
+++ app/src/main/java/org/maepaysoh/maepaysoh/db/MaepaysohDbHelper.java
@@ -28,6 +28,7 @@ public class MaepaysohDbHelper extends SQLiteOpenHelper {
   public static final String COLUMN_PARTY_REGION = "region";
   public static final String COLUMN_PARTY_CONTACT = "contact";
   public static final String COLUMN_PARTY_POLICY = "policy";
+  public static final String COLUMN_PARTY_HEADQUARTER = "party_headquarter";
   public static final int DATABASE_VERSION = 1;
   public static final String DATABASE_NAME = "maepaysoh.db";
   private static final String COMMA_SEP = ",";
