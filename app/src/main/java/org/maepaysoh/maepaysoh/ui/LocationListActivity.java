8de0c3cc6a08dadad26fb7c2cc4698514a0b868c
Ye Lin Aung <me@yelinaung.com>
2015-08-10 10:55:15 +0630

    Added toolbar to the location list activity.

--- app/src/main/java/org/maepaysoh/maepaysoh/ui/LocationListActivity.java
+++ app/src/main/java/org/maepaysoh/maepaysoh/ui/LocationListActivity.java
@@ -2,6 +2,9 @@ package org.maepaysoh.maepaysoh.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import org.maepaysoh.maepaysoh.R;
@@ -12,13 +15,37 @@ public class LocationListActivity extends BaseActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_geolocation);
+    setContentView(R.layout.activity_location);
+
+    Toolbar mToolbar = (Toolbar) findViewById(R.id.location_list_toolbar);
+    View mToolbarShadow = findViewById(R.id.location_list_toolbar_shadow);
+
+    mToolbar.setTitle(getString(R.string.LocationList));
+    hideToolBarShadowForLollipop(mToolbar, mToolbarShadow);
+
+    setSupportActionBar(mToolbar);
+
+    ActionBar mActionBar = getSupportActionBar();
+    if (mActionBar != null) {
+      // Showing Back Arrow  <-
+      mActionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
     ygnWestBtn = (Button) findViewById(R.id.yangon_west_btn);
+    ygnWestBtn.setText("Yangon West");
     ygnWestBtn.setOnClickListener(new View.OnClickListener() {
       @Override public void onClick(View v) {
         Intent goToDetail = new Intent(LocationListActivity.this, LocationDetailActivity.class);
         startActivity(goToDetail);
       }
     });
+  }
+
+  @Override public boolean onOptionsItemSelected(MenuItem item) {
+    int id = item.getItemId();
+    if (id == android.R.id.home) {
+      onBackPressed();
+    }
+    return super.onOptionsItemSelected(item);
   }
 }
