09555449f557e7b45486ec74b1b7c56d4f509d00
Ye Myat Thu <thu.yemyat@gmail.com>
2015-08-12 10:52:15 +0630

    Add API wrapper class and instantiate helper classes from it

---
+++ maepaysohsdk/src/main/java/org/maepaysoh/maepaysohsdk/MaePaySohApiWrapper.java
@@ -0,0 +1,21 @@
+package org.maepaysoh.maepaysohsdk;
+
+import android.content.Context;
+
+/**
+ * Created by yemyatthu on 8/12/15.
+ */
+public class MaePaySohApiWrapper {
+  protected Context mContext;
+  protected String mToken;
+  public MaePaySohApiWrapper(Context context){
+    mContext = context;
+  }
+
+  public void setApiKey(String token){
+    mToken = token;
+  }
+  public PartyAPIHelper getPartyApiHelper(){
+    return new PartyAPIHelper(mToken,mContext);
+  }
+}
