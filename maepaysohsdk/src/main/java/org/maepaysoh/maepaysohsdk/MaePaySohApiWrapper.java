31b657591254d731240156125dd53ba7ee3871eb
Ye Myat Thu <thu.yemyat@gmail.com>
2015-08-20 05:07:30 +0630

    Add public wrapper method for generating token keys inside MaePaySohAPIWrapper class.

--- maepaysohsdk/src/main/java/org/maepaysoh/maepaysohsdk/MaePaySohApiWrapper.java
+++ maepaysohsdk/src/main/java/org/maepaysoh/maepaysohsdk/MaePaySohApiWrapper.java
@@ -1,6 +1,8 @@
 package org.maepaysoh.maepaysohsdk;
 
 import android.content.Context;
+import org.maepaysoh.maepaysohsdk.models.TokenReturnObject;
+import retrofit.Callback;
 
 /**
  * Created by yemyatthu on 8/12/15.
@@ -17,9 +19,14 @@ public class MaePaySohApiWrapper {
     mToken = token;
   }
 
-  public void getTokenKeyAsync(String apiKey){
+  public String getTokenKey(String apiKey) {
+    return new TokenAPIHelper(mContext).getTokenKey(apiKey);
   }
 
+  public void getTokenKeyAsync(String apiKey, Callback<TokenReturnObject> returnObjectCallback){
+    new TokenAPIHelper(mContext).getTokenKeyAsync(apiKey,returnObjectCallback);
+  }
+  
   public PartyAPIHelper getPartyApiHelper() {
     return new PartyAPIHelper(mToken, mContext);
   }
